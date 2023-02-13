5feae27543131d8b1e28f23ce32c6b1020153c38
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-13 22:52:57 +0100

    Modify toDoForm

--- src/components/ToDoForm/ToDoForm.tsx
+++ src/components/ToDoForm/ToDoForm.tsx
@@ -3,10 +3,6 @@ import ToDoFormStyled from "./ToDoFormStyled";
 const ToDoForm = (): JSX.Element => {
   return (
     <ToDoFormStyled className="form-tasks">
-      <label htmlFor="id" aria-label="id" className="form-tasks__label-id">
-        ID
-      </label>
-      <input type="text" className="form-tasks__id" required />
       <label
         htmlFor="name"
         aria-label="name"
@@ -15,16 +11,8 @@ const ToDoForm = (): JSX.Element => {
         Name
       </label>
       <input type="text" className="form-tasks__name" required />
-      <label
-        htmlFor="status"
-        aria-label="task status"
-        className="form-tasks__label-isDone"
-      >
-        Status
-      </label>
-      <input type="text" className="form-tasks__isDone" required />
       <button type="submit" className="form-tasks__button">
-        Create task
+        Add task
       </button>
     </ToDoFormStyled>
   );
