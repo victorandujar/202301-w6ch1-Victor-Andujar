54a82b6023f9355b499083d1943a2b0e39c4135c
Víctor Andújar <victorandujarmurcia@gmail.com>
2023-02-13 22:05:59 +0100

    Add styles to ToDoForm component and add globalstyles

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import { store } from "./stores/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import GlobalStyles from "./GolbalStyles";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Provider store={store}>
       <App />
     </Provider>
